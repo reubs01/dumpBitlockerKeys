7e42143c2182b2d2769ba8a20d66ffbbc7c82008
Reuben B <112469788+reubs01@users.noreply.github.com>
2025-04-05 23:31:28 +1100

    Update dumpBitlockerKeys.ps1

--- dumpBitlockerKeys.ps1
+++ dumpBitlockerKeys.ps1
@@ -3,13 +3,10 @@
 
 Add-Type -AssemblyName System.Windows.Forms
 
-# Initialise an empty array to store the data
 $data = @()
 
-# Retrieve all bitLocker encrypted volumes
 $bitLockerVolumes = Get-BitLockerVolume
 
-# Retrieve information about all physical disks
 $physicalDisks = Get-PhysicalDisk
 
 # Loop through each physical disk to inspect its partitions
@@ -17,7 +14,6 @@ foreach ($disk in $physicalDisks) {
     # Obtain partitions for the current disk
     $partitions = Get-Partition -DiskNumber $disk.DeviceID
 
-    # Loop through each partition on the disk
     foreach ($partition in $partitions) {
         # Proceed only if the partition has an assigned drive letter
         if ($partition.DriveLetter) {
@@ -36,10 +32,8 @@ foreach ($disk in $physicalDisks) {
                     RecoveryKey = $recoveryKey.RecoveryPassword
                 }
 
-                # Add the custom object to the data array
                 $data += $obj
             } else {
-                # Notify for partitions without BitLocker
                 Write-Host "Drive $($partition.DriveLetter): is not BitLocker-protected."
             }
         }
@@ -53,19 +47,14 @@ if ($data.Count -gt 0) {
     $saveFileDialog.Filter = "CSV files (.csv)|.csv"
     $saveFileDialog.Title = "Select Location to Save BitLocker Recovery Keys"
 
-    # Show the save file dialog to the user
     $dialogResult = $saveFileDialog.ShowDialog()
 
-    # Check if the user has selected a file path
     if ($dialogResult -eq [System.Windows.Forms.DialogResult]::OK -and $saveFileDialog.FileName -ne "") {
-        # Export the collected data to the chosen CSV file
         $data | Export-Csv -Path $saveFileDialog.FileName -NoTypeInformation
         Write-Host "BitLocker Recovery Keys have been saved to $($saveFileDialog.FileName)"
     } else {
-        # Notify the user if the operation was cancelled or no file was selected for saving
         Write-Host "Operation cancelled or no file was selected for saving."
     }
 } else {
-    # Notify the user that no BitLocker encrypted drives were found and exit the script
     Write-Host "No BitLocker-encrypted drives were found."
 }
